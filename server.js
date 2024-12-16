47040e8048c9a5a4e8d85340ed14443279f6e94b
VarunParab <90065369+VarunParab@users.noreply.github.com>
2024-12-16 11:34:25 +0530

    update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const {app,server} = require('./lib/socket');
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"http://digitalvasai.com",
     credentials: true,
 }));
 app.use(cookieParser());
